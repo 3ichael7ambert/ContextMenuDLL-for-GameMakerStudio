3d0d5623995189271001c0fb55e8577ea7b0554b
3ichael7ambert <mlambertbusiness@gmail.com>
2024-07-27 08:19:05 -0600

    edits

--- ContextMenuDYLIB.m
+++ ContextMenuDYLIB.m
@@ -1,20 +1,22 @@
 #import <Cocoa/Cocoa.h>
 
-#define trace(...) { printf("[show_context_menu:%d] ", __LINE__); printf(__VA_ARGS__); printf("\n"); fflush(stdout); }
+#define trace(...) { NSLog(@"[show_context_menu:%d] %@", __LINE__, [NSString stringWithFormat:__VA_ARGS__]); }
+
+__attribute__((visibility("default"))) void ShowContextMenu(double x, double y);
 
 void ShowContextMenu(double x, double y) {
-    trace("Starting ShowContextMenu with x=%f, y=%f", x, y);
+    trace(@"Starting ShowContextMenu with x=%f, y=%f", x, y);
 
     NSMenu *menu = [[NSMenu alloc] initWithTitle:@"Context Menu"];
     if (!menu) {
-        trace("Failed to create popup menu");
+        trace(@"Failed to create popup menu");
         return;
     }
 
     [menu addItemWithTitle:@"Option 1" action:@selector(menuItemSelected:) keyEquivalent:@""];
     [menu addItemWithTitle:@"Option 2" action:@selector(menuItemSelected:) keyEquivalent:@""];
     [menu addItemWithTitle:@"Option 3" action:@selector(menuItemSelected:) keyEquivalent:@""];
-    
+
     NSPoint pt = NSMakePoint(x, y);
     NSEvent *event = [NSEvent mouseEventWithType:NSEventTypeRightMouseDown
                                          location:pt
@@ -25,15 +27,20 @@ void ShowContextMenu(double x, double y) {
                                       eventNumber:0
                                        clickCount:1
                                          pressure:1];
-    [NSMenu popUpContextMenu:menu withEvent:event forView:nil];
-    
-    trace("Menu displayed");
+    NSView *view = [[NSApp keyWindow] contentView];
+    if (view == nil) {
+        trace(@"Failed to get content view");
+        return;
+    }
+    [NSMenu popUpContextMenu:menu withEvent:event forView:view];
+
+    trace(@"Menu displayed");
 }
 
 @implementation NSApplication (ContextMenu)
 
 + (void)menuItemSelected:(NSMenuItem *)item {
-    trace("Menu item selected: %@", [item title]);
+    trace(@"Menu item selected: %@", [item title]);
     NSAlert *alert = [[NSAlert alloc] init];
     [alert setMessageText:[NSString stringWithFormat:@"%@ Selected", [item title]]];
     [alert runModal];
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ContextMenuDLL-for-GameMakerStudio
+
 A C++ DLL extension for Game Maker Studio to enable Windows right clicking.
 
 To compile .cpp to a .dll, you need to open Developer Terminal or Developer PowerShell, change to the directory and run the following command;
@@ -8,13 +9,15 @@ cl /LD ContextMenuDLL.cpp /Fe:ContextMenuDLL.dll /link User32.lib
 ```
 
 x64
+
 ```ps1
 cl /LD /Fe:ContextMenuDLL.dll ContextMenuDLL.cpp /link /MACHINE:X64 User32.lib
-```  
+```
 
+Mac OS
 
-
-Mac OS 
 ```bash
 clang -dynamiclib -o ContextMenu.dylib ContextMenuDYLIB.m -framework Cocoa
 ```
+
+NOTE: MAC OS isn't working atm
